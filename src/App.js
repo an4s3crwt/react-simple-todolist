eb265457d33c8df4d804bc2d1b7cc341d4923f8d
Ana Gilabert Lucas <anaymenta@gmail.com>
2024-11-10 14:18:13 +0100

    handleChange()

--- src/App.js
+++ src/App.js
@@ -8,13 +8,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from 'uuid';
 
 class App extends Component {
+  //gestionar las interacciones con la lista
+  state={
+    items: [],//almacena las tareas
+    id: 0, //con uuid se le dará un id único
+    item: '',//el valor se actualizará en tiempo real con handleChange
+    editItem:false //se inicializa en false porque en principio no se está editando 
+  }
+
+  /*
+  Manejador de eventos que se activa cada vez que ocurre un cambio en el campo
+  de entrada de texto, como cuando el usuario escribe en él
+  */
+  handleChange = (e) =>{//recibe el e vento 
+    this.setState({
+      item: e.target.value  //target representa el elemetno html donde ocurrió el evento(el input)
+      //value contiene el valor actual del texto(lo que el usuario ha escrito)
+    })
+  }
   render () {
   return (
     <div className="container">
       <div className="row">
         <div className="col-10 mx-auto col-md-8 mt-4">
           <h3 className="text-capitalize text-center">To Do Input</h3>
-        <TodoInput />
+          
+        <TodoInput item={this.state.item} handleChange={this.handleChange} />
         <TodoList />
         </div>
       </div>
